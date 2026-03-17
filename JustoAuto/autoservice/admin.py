2b24554e10c91b9fb6b82aa006e1030d47e6481d
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-17 13:24:53 +0200

    +klasės  - Service  - OrderLine  - Order  - Car

--- JustoAuto/autoservice/admin.py
+++ JustoAuto/autoservice/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Car, OrderLine, Order, Service
+
+admin.site.register(Car)
+admin.site.register(OrderLine)
+admin.site.register(Order)
+admin.site.register(Service)
+
+
--- JustoAuto/autoservice/models.py
+++ JustoAuto/autoservice/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class OrderLine(models.Model):
+    order = models.ForeignKey (to='Order', on_delete=models.SET_NULL, null=True, blank=True)
+    service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return f"{self.order} {self.service} {self.quantity}"
+
+class Order(models.Model):
+    date = models.DateField(null=True, blank=True)
+    car = models.ForeignKey(to="Car", on_delete=models.SET_NULL, null=True, blank=True)
+    # service = models.ManyToManyField(to="Service")
+    def __str__(self):
+        return f"{self.car} {self.date}"
+
+class Car(models.Model):
+    make = models.CharField()
+    model = models.CharField()
+    license_plate = models.CharField()
+    vin_code = models.CharField()
+    client_name = models.CharField()
+
+    def __str__(self):
+        return f"{self.make} {self.client_name} {self.license_plate}"
+
