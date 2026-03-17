f36cea807379fe249bad7c37b9530ca586b01108
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-17 13:55:37 +0200

    +TabularInline. Tai „atrakina“ galimybę viename lange redaguoti tėvinį objektą (Užsakymą) ir jo vaikus (Eilutes). Savo admin.py faile pridėk šį kodą

--- JustoAuto/autoservice/admin.py
+++ JustoAuto/autoservice/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from .models import Car, OrderLine, Order, Service
 
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderLineInline]
+
 admin.site.register(Car)
 admin.site.register(OrderLine)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Service)
+
+
 
 
