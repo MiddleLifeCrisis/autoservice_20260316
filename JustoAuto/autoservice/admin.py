a3be64d671074f66424d20af9e99dac47b48ea2a
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-18 11:17:54 +0200

    +visų modelių (klasių) pavadinimai būtų atvaizduojami teisingai (vienaskaita ir daugiskaita) +užsakymų sąraše būtų matomi automobilio ir datos stulpeliai

--- JustoAuto/autoservice/admin.py
+++ JustoAuto/autoservice/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 
 from .models import Car, OrderLine, Order, Service
 
-class OrderLineInline(admin.TabularInline):
-    model = OrderLine
-    extra = 1
-
 class OrderAdmin(admin.ModelAdmin):
-    inlines = [OrderLineInline]
+    list_display = ['date', 'car']
+
+
+
 
 admin.site.register(Car)
 admin.site.register(OrderLine)
--- JustoAuto/autoservice/models.py
+++ JustoAuto/autoservice/models.py
@@ -1,33 +1,54 @@
 from django.db import models
 
 class Service(models.Model):
-    name = models.CharField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(verbose_name="Paslaugos pav.")
+    price = models.DecimalField(verbose_name="Paslaugos kaina, EUR",
+                                max_digits=10,
+                                decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Paslauga'
+        verbose_name_plural = 'Paslaugos'
 
     def __str__(self):
         return f"{self.name}"
 
 class OrderLine(models.Model):
-    order = models.ForeignKey (to='Order', on_delete=models.SET_NULL, null=True, blank=True)
-    service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, null=True, blank=True)
-    quantity = models.IntegerField()
+    order = models.ForeignKey (to='Order', on_delete=models.SET_NULL, verbose_name="Užsakymas",
+                               null=True, blank=True)
+    service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, verbose_name="Paslauga",
+                                null=True, blank=True)
+    quantity = models.IntegerField(verbose_name="Užsakomas kiekis", default=1)
+
+    class Meta:
+        verbose_name = 'Užsakymo eilutė'
+        verbose_name_plural = 'Užsakymo eilutės'
+
     def __str__(self):
         return f"{self.order} {self.service} {self.quantity}"
 
 class Order(models.Model):
-    date = models.DateField(null=True, blank=True)
-    car = models.ForeignKey(to="Car", on_delete=models.SET_NULL, null=True, blank=True)
-    # service = models.ManyToManyField(to="Service", through='OrderLine')
+    date = models.DateField(null=True, blank=True, verbose_name="Data")
+    car = models.ForeignKey(to="Car", on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Automobilis")
+
+    class Meta:
+        verbose_name = 'Užsakymas'
+        verbose_name_plural = 'Užsakymai'
+
     def __str__(self):
         return f"{self.car} {self.date}"
 
 class Car(models.Model):
-    make = models.CharField()
-    model = models.CharField()
-    license_plate = models.CharField()
-    vin_code = models.CharField()
-    client_name = models.CharField()
+    make = models.CharField(verbose_name="Brand", max_length=50)
+    model = models.CharField(verbose_name="Model", max_length=50)
+    license_plate = models.CharField(verbose_name="Valstybinis numeris")
+    vin_code = models.CharField(verbose_name="VIN number")
+    client_name = models.CharField(verbose_name="Kliento vardas ir pavardė")
+
+    class Meta:
+        verbose_name = 'Automobilis'
+        verbose_name_plural = 'Automobiliai'
 
     def __str__(self):
-        return f"{self.make} {self.client_name} {self.license_plate}"
+        return f"{self.make}-{self.license_plate}:{self.client_name}"
 
