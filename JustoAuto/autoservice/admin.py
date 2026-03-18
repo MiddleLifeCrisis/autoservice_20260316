925837ef68f2cfc4f9627c11e5519e357de7a7a7
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-18 13:25:32 +0200

    +sąrašą įdėti filtrą pagal klientą, automobilio gamintoją ir modelį
    +Sutvarkytas automobilių laukų grupavimas (fieldsets)
    
    Pridėtas fieldsets automobilių įvedimo formos struktūrai
    
    Sugrupuoti modelio laukai admin panelėje geresniam matomumui

--- JustoAuto/autoservice/admin.py
+++ JustoAuto/autoservice/admin.py
@@ -12,12 +12,26 @@ class OrderAdmin(admin.ModelAdmin):
 
 class Automobiliai(admin.ModelAdmin):
     list_display = ['make', 'model', 'client_name', 'license_plate', 'vin_code']
+    list_filter = ['client_name', 'make', 'model']
 
+    fieldsets = [
+        ('Savininko informacija', {
+            'fields': ('client_name',)
+        }),
+        ('Automobilio duomenys', {
+            'fields': ('make', 'model', 'license_plate', 'vin_code'),
+            'description': 'Pagrindinė techninė informacija apie transporto priemonę.'
+        }),
+    ]
+
+
+class Paslaugos(admin.ModelAdmin):
+    list_display = ['name', 'price']
 
 admin.site.register(Car, Automobiliai)
 admin.site.register(OrderLine)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Service)
+admin.site.register(Service, Paslaugos)
 
 
 
