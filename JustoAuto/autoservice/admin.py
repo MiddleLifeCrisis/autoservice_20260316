790782e473c0072dac0fc0573a48be604eae0179
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-18 12:27:16 +0200

    +automobilių sąraše būtų matomi gamintojo, modelio, kliento, valstybinio numerio ir VIN numerio stulpeliai

--- JustoAuto/autoservice/admin.py
+++ JustoAuto/autoservice/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 
 from .models import Car, OrderLine, Order, Service
 
+class OrderAdminInline(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['date', 'car']
+    inlines = [OrderAdminInline]
+
+class Automobiliai(admin.ModelAdmin):
+    list_display = ['make', 'model', 'client_name', 'license_plate', 'vin_code']
 
 
-
-
-admin.site.register(Car)
+admin.site.register(Car, Automobiliai)
 admin.site.register(OrderLine)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Service)
