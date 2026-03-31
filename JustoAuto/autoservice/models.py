d7b2d0436299c78e70855f6f159bb638d30e442d
Justas Osipovas <justas.osipovas@gmail.com>
2026-03-31 12:08:48 +0300

    +Prisegama nuotrauka aumotiškai sumažinama ir padaroma kvadratine. Ji atvaizduojama ir prie komentarų

--- JustoAuto/autoservice/models.py
+++ JustoAuto/autoservice/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from tinymce.models import HTMLField
+from PIL import Image
 from django.core.paginator import Paginator
 
 class Service(models.Model):
@@ -105,6 +106,19 @@ class OrderReview(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="profile_pics", null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.photo:
+            img = Image.open(self.photo.path)
+            min_side = min(img.width, img.height)
+            left = (img.width - min_side) // 2
+            top = (img.height - min_side) // 2
+            right = left + min_side
+            bottom = top + min_side
+            img = img.crop((left, top, right, bottom))
+            img = img.resize((300, 300), Image.LANCZOS)
+            img.save(self.photo.path)
 
     def __str__(self):
         return f"{self.user.username} profilis"
